e806ba74fe2c287868ef3f08a8d960ec560f8570
Alexandre Fernandes <alexandrefer1231@gmail.com>
2025-08-08 22:28:14 -0300

    add: dashboard started and created component

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Sidebar from './components/sidebar.jsx'
+import Sidebar from '../../components/Sidebar.jsx'
+import DashboardView from './components/DashboardView.jsx'
 
 const Dashboard = () => {
   return (
-    <div className="h-[100vh] flex flex-col">
-      <Sidebar className="w-100px" />
-      <h1>Dashboard</h1>
-      <Link to="/">retornar a página inicial</Link>
+    <div className="h-[100vh] flex">
+      <Sidebar size="w-1/4"/>
+      <DashboardView size="w-3/4"/>
     </div>
   )
 }
